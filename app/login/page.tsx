d767f9cf561d5f4fbb26c15bbf002d2eb66a6f1a
kimdaae328 <kimdaae328@gmail.com>
2024-05-17 13:40:55 +0900

    .

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,32 +1,38 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import styles from '../../styles/login.module.css';
 
-interface ButtonProps {
-  children: ReactNode;
-  line?: boolean;
-  color?: string;
-}
-
-const Button: React.FC<ButtonProps> = ({ children, line, color }) => {
-  const buttonStyle: React.CSSProperties = {
+function Button({ children, line, color }) {
+  const buttonStyle = {
     backgroundColor: color || 'initial',
     border: line ? '1px solid #5f0080' : 'none',
   };
 
   return (
-    <button style={buttonStyle} className={styles.button}>
+    <button style={buttonStyle}>
       {children}
     </button>
   );
-};
+}
 
-export default function LoginPage() {
+export default function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-      <Button color="blue" line={true}>
-        Login
-      </Button>
+    <div className={styles.loginArea}>
+      <span className={styles.loginTitle}>로그인</span>
+
+      <form action="" className={styles.loginForm}>
+        <input type="text" placeholder='아이디를 입력해주세요' />
+        <input type="password" placeholder='비밀번호를 입력해주세요' />
+
+        <div className={styles.searchBtn}>
+          <button>아이디 찾기</button>
+          <button>비밀번호 찾기</button>
+        </div>
+
+        <div className={styles.loginBtn}>
+            <Button color="#5f0080">로그인</Button>
+            <Button line>회원가입</Button>
+        </div>
+      </form>
     </div>
   );
 }
