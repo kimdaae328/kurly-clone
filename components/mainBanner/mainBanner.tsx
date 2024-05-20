decb51632dab1e0da54b7eadb53d16e96e67e180
kimdaae <kimdaae328@gmail.com>
2024-05-20 20:48:19 +0900

    일단 문제해결?

--- components/mainBanner/mainBanner.tsx
+++ components/mainBanner/mainBanner.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import styles from "../../styles/mainBanner/mainBanner.module.css";
-// import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 export default function Mainbanner() {
-  // const settings = {
-  //   dots: true,
-  //   infinite: true,
-  //   speed: 500,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  // };
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
   return (
     <div className={styles.mainbanner}>
-      <img src="https://res.cloudinary.com/dpr8m7xge/image/upload/v1715841193/xt1cjirpj4t5564vwrzz.jpg" alt="" />
-      {/* <div className="slider-container">
+      {/* <img src="https://res.cloudinary.com/dpr8m7xge/image/upload/v1715841193/xt1cjirpj4t5564vwrzz.jpg" alt="" /> */}
+      <div className="slider-container">
         <Slider {...settings}>
           <div>
             <h3>1</h3>
@@ -37,8 +37,8 @@ export default function Mainbanner() {
           <div>
             <h3>6</h3>
           </div>
-        </Slider> */}
-      {/* </div> */}
+        </Slider>
+      </div>
     </div>
   )
 }
