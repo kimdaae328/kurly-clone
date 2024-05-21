0ec202238fee42ddf4ae5866ffdd33bebf2bec19
kimdaae328 <kimdaae328@gmail.com>
2024-05-21 16:21:32 +0900

    이전, 다음 버튼 생성 - 디자인은 아직

--- components/mainBanner/mainBanner.tsx
+++ components/mainBanner/mainBanner.tsx
@@ -12,6 +12,10 @@ export default function Mainbanner() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    arrows: true,
+    autoplay : true,
+  //   prevArrow : "<button type='button' class='slick-prev'>Previous</button>",
+	// nextArrow : "<button type='button' class='slick-next'>Next</button>",
   };
 
   return (
@@ -19,23 +23,24 @@ export default function Mainbanner() {
       <div className={styles.sliderContainer}>
         <Slider {...settings} className={styles.slickSlider}>
           <div className={styles.slickSlide}>
-            <img src="https://res.cloudinary.com/dpr8m7xge/image/upload/v1715841193/xt1cjirpj4t5564vwrzz.jpg" alt="" />
+            <img src="https://res.cloudinary.com/dpr8m7xge/image/upload/v1716253626/b4vw83xbqecqrcegfmac.jpg" alt="" />
             <h3>1</h3>
           </div>
           <div className={styles.slickSlide}>
+            <img src="https://res.cloudinary.com/dpr8m7xge/image/upload/v1716253626/pdimtqx6najh34ccxsis.jpg" alt="" />
             <h3>2</h3>
           </div>
           <div className={styles.slickSlide}>
+            <img src="https://res.cloudinary.com/dpr8m7xge/image/upload/v1716253626/rsyvu5fwmgkm0w9ylcsh.jpg" alt="" />
             <h3>3</h3>
           </div>
           <div className={styles.slickSlide}>
+            <img src="https://res.cloudinary.com/dpr8m7xge/image/upload/v1716253626/bzk89qqnl1eeyylwh3no.jpg" alt="" />
             <h3>4</h3>
           </div>
           <div className={styles.slickSlide}>
+            <img src="https://res.cloudinary.com/dpr8m7xge/image/upload/v1715841193/xt1cjirpj4t5564vwrzz.jpg" alt="" />
             <h3>5</h3>
-          </div>
-          <div className={styles.slickSlide}>
-            <h3>6</h3>
           </div>
         </Slider>
       </div>
