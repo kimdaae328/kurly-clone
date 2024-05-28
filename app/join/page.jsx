16d61a106c8724cb9a1358b2cab8236eec30f347
kimdaae328 <kimdaae328@gmail.com>
2024-05-28 11:20:59 +0900

    회원가입

--- app/join/page.jsx
+++ app/join/page.jsx
@@ -7,31 +7,32 @@ export default function JoinPage() {
       <h2 className={styles.pageTitle}>회원가입</h2>
 
       <div className={styles.joinFromWrap}>
-        <p className={styles.essential}><span>*</span>필수입력사항</p>
+        <p className={styles.essentialWrap}><span className={styles.essential}>*</span>필수입력사항</p>
 
         <form className={styles.joinFrom}>
           <div className={styles.joinItem}>
-            <label htmlFor="username">아이디:</label>
+            <label htmlFor="username">아이디 <span className={styles.essential}>*</span></label>
             <input type="text" id="username" name="username" placeholder="아이디를 입력해주세요" />
             <button className={styles.btnLine}>중복확인</button>
           </div>
           <div className={styles.joinItem}>
-            <label htmlFor="password">비밀번호:</label>
+            <label htmlFor="password">비밀번호 <span className={styles.essential}>*</span></label>
             <input type="password" id="password" name="password" placeholder="비밀번호를 입력해주세요" />
           </div>
           <div className={styles.joinItem}>
-            <label htmlFor="passwordConfirm">비밀번호확인:</label>
+            <label htmlFor="passwordConfirm">비밀번호확인 <span className={styles.essential}>*</span></label>
             <input type="passwordConfirm" id="passwordConfirm" name="passwordConfirm" />
           </div>
           <div className={styles.joinItem}>
-            <label htmlFor="email">이메일:</label>
+            <label htmlFor="email">이메일 <span className={styles.essential}>*</span></label>
             <input type="text" id="email" name="email" placeholder="예: marketkurly@kurly.com" />
             <button className={styles.btnLine}>중복확인</button>
           </div>
           <div className={styles.joinItem}>
-            <label htmlFor="mobileNumber">휴대폰:</label>
+            <label htmlFor="mobileNumber">휴대폰 <span className={styles.essential}>*</span></label>
             <input type="tel" id="mobileNumber" name="mobileNumber" placeholder="숫자만 입력해주세요." />
           </div>
+          <button type="submit" className={styles.btnColor}>가입하기</button>
         </form>
       </div>
     </div>
