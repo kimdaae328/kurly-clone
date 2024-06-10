ab4022b321ea8146fc799bce52f5289072d9d9da
kimdaae328 <kimdaae328@gmail.com>
2024-06-10 16:04:25 +0900

    마이페이지 시작

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import db from '@/lib/db';
+import getSession from '@/lib/session';
+
+async function getUser(){
+  const session = getSession();
+  if(session.id){
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      }
+    })
+  }
+}
 
 export default function Mypage() {
+  const user = await getUser()
   return (
     <h2>회원가입을 축하합니다.</h2>
   )
