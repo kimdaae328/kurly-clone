2fac65fa65ee8c0deec9cbf592cdbd6623d0684c
kimdaae328 <kimdaae328@gmail.com>
2024-06-11 14:18:02 +0900

    로그아웃 되돌리기
    로그아웃 되돌리기

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,32 +1,35 @@
-import React from 'react'
-import db from '@/lib/db';
-import getSession from '@/lib/session';
+import getSession from "@/lib/session";
+import { notFound, redirect } from "next/navigation";
 
-async function getUser(){
-  const session = getSession();
-  if(session.id){
+async function getUser() {
+  const session = await getSession();
+  if (session.id) {
     const user = await db.user.findUnique({
       where: {
         id: session.id,
-      }
-    })
+      },
+    });
+    if (user) {
+      return user;
+    }
   }
+  notFound();
 }
 
-export default function Mypage() {
-  const user = await getUser()
+export default async function Mypage() {
+  const user = await getUser();
+  const logOut = async () => {
+    "use server";
+    const session = await getSession();
+    await session.destroy();
+    redirect("/");
+  };
   return (
-<<<<<<< HEAD
     <div>
       <h1>{user?.username}님 회원가입을 완료했습니다!</h1>
       <form action={logOut}>
-        <button>로그아웃</button>
+        <button>Log out</button>
       </form>
     </div>
   );
 }
-=======
-    <h2>회원가입을 축하합니다.</h2>
-  )
-}
->>>>>>> parent of 6a1fcce (로그아웃 완료)
