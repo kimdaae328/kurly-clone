3e719d6d744d810546f63e53cbac434d48421983
kimdaae328 <kimdaae328@gmail.com>
2024-06-11 14:20:02 +0900

    완전 삭제
    완전 삭제

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,36 +1,9 @@
-import db from "@/lib/db";
-import getSession from "@/lib/session";
-import { notFound, redirect } from "next/navigation";
+import React from 'react'
 
-async function getUser() {
-  const session = await getSession();
-  if (session.id) {
-    const user = await db.user.findUnique({
-      where: {
-        id: session.id,
-      },
-    });
-    if (user) {
-      return user;
-    }
-  }
-  notFound();
-}
-
-export default async function Mypage() {
-  const user = await getUser();
-  const logOut = async () => {
-    "use server";
-    const session = await getSession();
-    await session.destroy();
-    redirect("/");
-  };
+export default function Mypage() {
   return (
-    <div>
-      <h1>{user?.username}님 회원가입을 완료했습니다!</h1>
-      <form action={logOut}>
-        <button>Log out</button>
-      </form>
-    </div>
-  );
+
+    <h2>회원가입을 축하합니다.</h2>
+  )
 }
+
