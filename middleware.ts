36ceffef8d84c556e5503419aa89964cfe45ff94
kimdaae328 <kimdaae328@gmail.com>
2024-06-11 09:48:20 +0900

    삭제

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from "next/server";
-
-export async function middleware(request: NextRequest) {
-  console.log("hello");
-}
-
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-};
