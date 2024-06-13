2c44a1955bcf59c7b6ae31fbcc7e1964977beec0
kimdaae <kimdaae328@gmail.com>
2024-06-13 19:00:45 +0900

    탙퇴완료

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -25,11 +25,29 @@ export default async function Mypage() {
     await session.destroy();
     redirect("/");
   };
+  
+  const deleteAccount = async () => {
+    "use server";
+    const session = await getSession();
+    if (session.id) {
+      await db.user.delete({
+        where: {
+          id: session.id,
+        },
+      });
+      await session.destroy();
+      redirect("/");
+    }
+  };
+
   return (
     <div>
       <h1>Welcome! {user?.username}!</h1>
       <form action={logOut}>
         <button>로그아웃</button>
+      </form>
+      <form action={deleteAccount}>
+        <button>회원탈퇴</button>
       </form>
     </div>
   );
